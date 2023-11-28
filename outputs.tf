b98066adb7c455daa90fc722ea4d7f2bb13b1a06
Patrick Brennan <patrickabrennan@gmail.com>
2023-11-28 10:23:23 -0500

    Update outputs.tf
    Adjusted output.tf to account for for_each use

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,31 @@
-output "public_dns_name" {
-  description = "Public DNS name of load balancer"
-  value       = module.elb_http.elb_dns_name
+#Commented out items now that we are using for_each on 11/28/2023
+#output "public_dns_name" {
+#  description = "Public DNS name of load balancer"
+#  value       = module.elb_http.elb_dns_name
+#}
+
+#output "vpc_arn" {
+#  description = "ARN of the vpc"
+#  value       = module.vpc.vpc_arn
+#}
+
+#output "instance_ids" {
+#  description = "IDs of EC2 instances"
+#  value       = aws_instance.app.*.id
+#}
+#Added below 11/28/2023
+output "public_dns_names" {
+  description = "Public DNS names of the load balancers for each project."
+  value       = { for p in sort(keys(var.project)) : p => module.elb_http[p].elb_dns_name }
 }
 
-output "vpc_arn" {
-  description = "ARN of the vpc"
-  value       = module.vpc.vpc_arn
+output "vpc_arns" {
+  description = "ARNs of the vpcs for each project."
+  value       = { for p in sort(keys(var.project)) : p => module.vpc[p].vpc_arn }
 }
 
 output "instance_ids" {
-  description = "IDs of EC2 instances"
-  value       = aws_instance.app.*.id
+  description = "IDs of EC2 instances."
+  value       = { for p in sort(keys(var.project)) : p => module.ec2_instances[p].instance_ids }
 }
+
