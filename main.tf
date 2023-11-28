48c716abe9a83dea55a69e36d25cb2beeb6fd2d8
Patrick Brennan <patrickabrennan@gmail.com>
2023-11-28 11:21:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -109,8 +109,8 @@ module "elb_http" {
   subnets         = module.vpc.public_subnets
 
   #Added 11/28/2023
-  number_of_instances = length(module.aws-instance[each.key].instance_ids)
-  instances           = module.aws-instance[each.key].instance_ids
+  number_of_instances = length(modules.aws-instance[each.key].instance_ids)
+  instances           = modules.aws-instance[each.key].instance_ids
 
   #Commented out 11/28/2023
   #number_of_instances = length(aws_instance.app)
