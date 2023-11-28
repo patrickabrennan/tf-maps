595cee4ddfa4b5893f4391f2ba6c954ad848660d
Patrick Brennan <patrickabrennan@gmail.com>
2023-11-28 11:28:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ module "elb_http" {
   #Added 11/28/2023
   number_of_instances = length(module.ec2_instances[each.key].instance_ids)
   instances           = module.ec2_instances[each.key].instance_ids
-
+ 
   #Commented out 11/28/2023
   #number_of_instances = length(aws_instance.app)
   #instances           = aws_instance.app.*.id
