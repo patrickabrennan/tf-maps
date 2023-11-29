3f98b4d0ea6883bab657596b297c07856fffefa9
Patrick Brennan <patrickabrennan@gmail.com>
2023-11-29 13:30:29 -0500

    Update main.tf

--- modules/aws-instance/main.tf
+++ modules/aws-instance/main.tf
@@ -17,26 +17,25 @@ resource "aws_instance" "app" {
   subnet_id              = var.subnet_ids[count.index % length(var.subnet_ids)]
   vpc_security_group_ids = var.security_group_ids
 
-  #added public IP
-  associate_public_ip_address = true
+  #added public IP 11/29/2023
+  #associate_public_ip_address = true
 
   user_data = <<-EOF
     #!/bin/bash
-    echo "Installing Pat's Google Maps Application"
-    yum update -y
-    yum install docker -y
-    systemctl start docker 
-    systemctl enable docker
-    chmod 666 /var/run/docker.sock
-    docker run --rm -d -p 80:80 -p 443:443 --name myweb patrickabrennan/myweb
-    echo "Completed Installing Pat's Google Maps Application"
-    # #!/bin/bash
-    #sudo yum update -y
-    #sudo yum install httpd -y
-    #sudo systemctl enable httpd
-    #sudo systemctl start httpd
-    #echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
+    #echo "Installing Pat's Google Maps Application"
+    #yum update -y
+    #yum install docker -y
+    #systemctl start docker 
+    #systemctl enable docker
+    #chmod 666 /var/run/docker.sock
+    #docker run --rm -d -p 80:80 -p 443:443 --name myweb patrickabrennan/myweb
+    #echo "Completed Installing Pat's Google Maps Application"
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+  EOF
 
   tags = {
     Terraform   = "true"
