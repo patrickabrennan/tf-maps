c17dec9ae1e8c4da5630ba5497e612d71913b638
Patrick Brennan <patrickabrennan@gmail.com>
2024-02-09 12:39:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@
 #}
 
 provider "aws" {
+  assume_role_with_web_identity {
+    role_arn                = "arn:aws:iam::285942769742:role/tfc-workload-identity"
+    #session_name            = "SESSION_NAME"
+    web_identity_token_file = "identity_token.aws.jwt_filename"
+  }
+}
+
+
+provider "aws" {
   region = var.aws_region
   assume_role {
     role_arn     = "arn:aws:iam::285942769742:role/tfc-workload-identity"
