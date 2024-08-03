da503b05d98b624370c7a082b9418bc05544eba2
Patrick Brennan <patrickabrennan@gmail.com>
2024-08-03 10:07:36 -0400

    Update main.tf

--- modules/aws-instance/main.tf
+++ modules/aws-instance/main.tf
@@ -23,12 +23,12 @@ resource "aws_instance" "app" {
   user_data = <<-EOF
     #!/bin/bash
     echo "Installing Pat's Google Maps Application"
-    sudo yum update -y
-    sudo yum install docker -y
-    sudo systemctl start docker 
-    sudo systemctl enable docker
-    sudo chmod 666 /var/run/docker.sock
-    docker run --rm -d -p 80:80 -p 443:443 --name myweb patrickabrennan/myweb
+    sudo bash -c 'yum update -y'
+    sudo bash -c 'yum install docker -y'
+    sudo bash -c 'systemctl start docker' 
+    sudo bash -c 'systemctl enable docker'
+    sudo bash -c 'chmod 666 /var/run/docker.sock'
+    docker bash -c 'run --rm -d -p 80:80 -p 443:443 --name myweb patrickabrennan/myweb'
     echo "Completed Installing Pat's Google Maps Application"
     #sudo yum update -y
     #sudo yum install httpd -y
