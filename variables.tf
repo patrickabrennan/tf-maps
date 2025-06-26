c64aa2891bb291b087877f304f0c1b55449ba548
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-26 13:23:19 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,11 +4,6 @@ variable "ssh_key_name" {
   type        = string
 }
 
-variable "ssh_public_key_path" {
-  description = "Path to your SSH public key"
-  type        = string
-}
-
 
 
 variable "aws_region" {
