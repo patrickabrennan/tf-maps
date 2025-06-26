1eca3ccfebca4b39ad930b6bf216cd9873ef400f
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-25 22:18:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -156,14 +156,14 @@ module "elb_http" {
 }
 
 #ADDED  12-19-2023
-resource "aws_route53_record" "tf-demo" {
-  zone_id = "Z08017432VFWFXO6IWHIK"
-  #name    = var.aws_dns_name
-  name = "maps.demo.pabrennan.com"
-  type    = "CNAME"
-  ttl     = 300
-  records = [module.elb_http["client-webapp"].elb_dns_name]
-}
+##resource "aws_route53_record" "tf-demo" {
+##  zone_id = "Z08017432VFWFXO6IWHIK"
+##  #name    = var.aws_dns_name
+##  name = "maps.demo.pabrennan.com"
+##  type    = "CNAME"
+##  ttl     = 300
+##  records = [module.elb_http["client-webapp"].elb_dns_name]
+##}
 
 
 #NEW EC2_INSTANCES MODULE 6/25/25
