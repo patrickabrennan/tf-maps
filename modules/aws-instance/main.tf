2112feb6d8172b94a270048c8c00fed0a04c1b01
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-26 12:08:20 -0400

    Update main.tf

--- modules/aws-instance/main.tf
+++ modules/aws-instance/main.tf
@@ -8,6 +8,12 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+#ADDED 6/26/25
+resource "aws_key_pair" "deployer" {
+  key_name   = "pat-key"
+  public_key = file("~/.ssh/id_rsa.pub") # Make sure this file exists
+}
+
 resource "aws_instance" "app" {
   count = var.instance_count
 
@@ -19,22 +25,25 @@ resource "aws_instance" "app" {
 
   #added public IP 12/15/2023
   associate_public_ip_address = true
+  
+  #ADDED 6/26/2025
+  key_name = aws_key_pair.deployer.key_name
 
   user_data = <<-EOF
-    !/bin/bash
-    #echo "Installing Pat's Google Maps Application"
-    #sudo bash -c 'yum update -y'
-    #sudo bash -c 'yum install docker -y'
-    #sudo bash -c 'systemctl start docker' 
-    #sudo bash -c 'systemctl enable docker'
-    #sudo bash -c 'chmod 666 /var/run/docker.sock'
-    #docker run --rm -d -p 80:80 -p 443:443 --name myweb patrickabrennan/myweb
-    #echo "Completed Installing Pat's Google Maps Application"
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    #!/bin/bash
+    echo "Installing Pat's Google Maps Application"
+    sudo bash -c 'yum update -y'
+    sudo bash -c 'yum install docker -y'
+    sudo bash -c 'systemctl start docker' 
+    sudo bash -c 'systemctl enable docker'
+    sudo bash -c 'chmod 666 /var/run/docker.sock'
+    docker run --rm -d -p 80:80 -p 443:443 --name myweb patrickabrennan/myweb
+    echo "Completed Installing Pat's Google Maps Application"
+    #sudo yum update -y
+    #sudo yum install httpd -y
+    #sudo systemctl enable httpd
+    #sudo systemctl start httpd
+    #echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
   EOF
 
   tags = {
