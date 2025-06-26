f6cfccb32a3885f007f4b625f24516fc0c1b2706
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-26 13:21:06 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,6 @@ variable "ssh_key_name" {
 variable "ssh_public_key_path" {
   description = "Path to your SSH public key"
   type        = string
-  default     = "/Users/patrick.brennan/.ssh/id_rsa.pub"
 }
 
 
