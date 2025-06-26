b3cd555180aff8a166fb7be40f31252082cfa96e
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-25 22:16:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -139,7 +139,7 @@ module "elb_http" {
     instance_protocol  = "HTTP"
     lb_port            = "443"
     lb_protocol        = "HTTPS"
-    ssl_certificate_id = "c8842388-a5db-414d-9378-a98d4642455b"
+    ssl_certificate_id = "arn:aws:acm:us-east-2:285942769742:certificate/c8842388-a5db-414d-9378-a98d4642455b"
   }]
 
   health_check = {
