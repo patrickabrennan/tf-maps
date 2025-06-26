04d3afadca4c4007b7f53fb4ed1670729219ddfa
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-26 09:27:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -156,33 +156,16 @@ module "elb_http" {
 }
 
 #ADDED 6/26/2025
-data "aws_route53_zone" "primary" {
-  name         = "pabrennan.com"
-  private_zone = false
-}
+resource "aws_route53_record" "app_dns" {
+  for_each = { for k, v in var.project : k => v if v.environment == "prod" } # Optional filter
 
-#resource "aws_route53_record" "app_dns" {
-#  for_each = { for k, v in var.project : k => v if v.environment == "prod" } # Filter if needed
-
-  zone_id = "Z08017432VFWFXO6IWHIK"
-  #zone_id = data.aws_route53_zone.primary.zone_id
-  name    = "maps-${each.key}.demo.pabrennan.com"
-  #name    = "maps-${each.key}.${data.aws_route53_zone.primary.name}"
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "maps-${each.key}.demo.${data.aws_route53_zone.primary.name}"
   type    = "CNAME"
   ttl     = 300
+
   records = [module.elb_http[each.key].elb_dns_name]
 }
-
-#ADDED  12-19-2023
-##resource "aws_route53_record" "tf-demo" {
-##  zone_id = "Z08017432VFWFXO6IWHIK"
-##  #name    = var.aws_dns_name
-##  name = "maps.demo.pabrennan.com"
-##  type    = "CNAME"
-##  ttl     = 300
-##  records = [module.elb_http["client-webapp"].elb_dns_name]
-##}
-
 
 #NEW EC2_INSTANCES MODULE 6/25/25
 module "ec2_instances" {
