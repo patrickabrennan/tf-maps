223b0ba67e2cab01f536b74f6c9c1b56b016e872
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-27 11:17:29 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,11 +9,6 @@ output "maps_dns_record_fqdn" {
   description = "FQDN of the Route 53 record for maps"
 }
 
-
-
-
-
-
 output "maps_elb_dns_name" {
   description = "The AWS-assigned DNS name of the maps load balancer"
   value       = module.elb_http["backend"].elb_dns_name
@@ -24,13 +19,12 @@ output "maps_elb_zone_id" {
   value       = module.elb_http["backend"].elb_zone_id
 }
 
-
 output "vpc_arns" {
-  description = "ARNs of the vpcs for each project."
-  value       = { for p in sort(keys(var.project)) : p => module.vpc[p].vpc_arn }
+  value = { for p in sort(keys(module.vpc)) : p => module.vpc[p].vpc_arn }
+  description = "VPC ARNs per project"
 }
 
 output "instance_ids" {
-  description = "IDs of EC2 instances."
-  value       = { for p in sort(keys(var.project)) : p => module.ec2_instances[p].instance_ids }
+  value = { for p in sort(keys(module.ec2_instances)) : p => module.ec2_instances[p].instance_ids }
+  description = "EC2 instance IDs per project"
 }
