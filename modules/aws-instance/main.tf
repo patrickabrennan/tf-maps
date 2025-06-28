fcaea5dade8c94ff4d22e534dbb480449c6d4082
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-28 09:34:45 -0400

    Update main.tf

--- modules/aws-instance/main.tf
+++ modules/aws-instance/main.tf
@@ -1,11 +1,41 @@
-#ADDED 6/26/2025
-variable "ssh_key_name" {
-  description = "Name of the SSH key pair"
-  type        = string
-  default     = ""
+#ADDED 6/28/2025
+variable "instance_count" {}
+variable "subnet_ids" {}
+variable "instance_type" {}
+variable "security_group_ids" {}
+variable "project_name" {}
+variable "environment" {}
+variable "ssh_key_name" {}
+variable "associate_public_ip_address" {
+  type    = bool
+  default = true
 }
 
+resource "aws_instance" "this" {
+  count = var.instance_count
 
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = var.instance_type
+  subnet_id                   = element(var.subnet_ids, count.index % length(var.subnet_ids))
+  vpc_security_group_ids      = var.security_group_ids
+  key_name                    = var.ssh_key_name
+  associate_public_ip_address = var.associate_public_ip_address
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              usermod -a -G docker ec2-user
+              systemctl enable docker
+              EOF
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-instance-${count.index}"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
@@ -17,41 +47,60 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_instance" "app" {
-  count = var.instance_count
 
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
+#variable "ssh_key_name" {
+#  description = "Name of the SSH key pair"
+#  type        = string
+#  default     = ""
+#}
 
-  subnet_id              = var.subnet_ids[count.index % length(var.subnet_ids)]
-  vpc_security_group_ids = var.security_group_ids
+
+
+#data "aws_ami" "amazon_linux" {
+#  most_recent = true
+#  owners      = ["amazon"]
+
+#  filter {
+#    name   = "name"
+#    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#  }
+#}
+
+#resource "aws_instance" "app" {
+#  count = var.instance_count
+
+#  ami           = data.aws_ami.amazon_linux.id
+#  instance_type = var.instance_type
+
+#  subnet_id              = var.subnet_ids[count.index % length(var.subnet_ids)]
+#  vpc_security_group_ids = var.security_group_ids
 
   #added public IP 12/15/2023
-  associate_public_ip_address = true
+# associate_public_ip_address = true
 
   #ADDED 6/26/2025
-  key_name = var.ssh_key_name
-  user_data = <<-EOF
-  #!/bin/bash
-  echo "Installing Pat's Google Maps Application"
+#  key_name = var.ssh_key_name
+#  user_data = <<-EOF
+#  #!/bin/bash
+#  echo "Installing Pat's Google Maps Application"
 
   # Update and install Docker properly
-  sudo yum update -y
-  sudo amazon-linux-extras install docker -y
-  sudo systemctl start docker
-  sudo systemctl enable docker
+ # sudo yum update -y
+ # sudo amazon-linux-extras install docker -y
+ # sudo systemctl start docker
+ # sudo systemctl enable docker
 
   # Add ec2-user to docker group
-  sudo usermod -aG docker ec2-user
+  #sudo usermod -aG docker ec2-user
 
   # Allow docker socket access (optional, may be insecure)
-  sudo chmod 666 /var/run/docker.sock
+  #sudo chmod 666 /var/run/docker.sock
 
   # Run your Docker container
-  docker run --rm -d -p 80:80 -p 443:443 --name myweb patrickabrennan/myweb
+  #docker run --rm -d -p 80:80 -p 443:443 --name myweb patrickabrennan/myweb
 
-  echo "Completed Installing Pat's Google Maps Application"
-  EOF
+  #echo "Completed Installing Pat's Google Maps Application"
+  #EOF
 
   #user_data = <<-EOF
   #  #!/bin/bash
@@ -70,9 +119,9 @@ resource "aws_instance" "app" {
     #echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
   #EOF
 
-  tags = {
-    Terraform   = "true"
-    Project     = var.project_name
-    Environment = var.environment
-  }
-}
+  #tags = {
+  #  Terraform   = "true"
+  #  Project     = var.project_name
+  #  Environment = var.environment
+ # }
+#}
