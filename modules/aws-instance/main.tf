6ba65a272f98d8e0742ae2b691efb6959563ad9a
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-28 10:12:44 -0400

    Update main.tf

--- modules/aws-instance/main.tf
+++ modules/aws-instance/main.tf
@@ -11,14 +11,23 @@
 #  default = true
 #}
 
-resource "aws_instance" "this" {
-  count = var.instance_count
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = var.instance_type
-  subnet_id                   = element(var.subnet_ids, count.index % length(var.subnet_ids))
-  vpc_security_group_ids      = var.security_group_ids
-  key_name                    = var.ssh_key_name
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "this" {
+  count                     = var.instance_count
+  ami                       = data.aws_ami.amazon_linux.id
+  instance_type             = var.instance_type
+  subnet_id                 = element(var.subnet_ids, count.index % length(var.subnet_ids))
+  vpc_security_group_ids    = var.security_group_ids
+  key_name                  = var.ssh_key_name
   associate_public_ip_address = var.associate_public_ip_address
 
   user_data = <<-EOF
@@ -37,15 +46,7 @@ resource "aws_instance" "this" {
   }
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
 
 
 #variable "ssh_key_name" {
