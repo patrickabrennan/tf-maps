8a286051536979afd0305ba3455ae6993cba1f81
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-28 09:55:07 -0400

    Update outputs.tf

--- modules/aws-instance/outputs.tf
+++ modules/aws-instance/outputs.tf
@@ -1,4 +1,9 @@
 output "instance_ids" {
-  description = "IDs of EC2 instances"
-  value       = aws_instance.app.*.id
+  value = aws_instance.this.*.id
 }
+
+
+#output "instance_ids" {
+#  description = "IDs of EC2 instances"
+#  value       = aws_instance.app.*.id
+#}
