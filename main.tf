ae4a35cca3b24117f7f2cb5ea0f169fb1c963d4e
Patrick Brennan <patrickabrennan@gmail.com>
2025-06-28 01:27:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,26 +46,6 @@ module "vpc" {
   map_public_ip_on_launch = false
 }
 
-#NEW VPC Module ADDED 6/25/25
-#module "vpc" {
-#  source  = "terraform-aws-modules/vpc/aws"
-#  version = "3.14.2"
-
-#  #for_each = { for p in local.flattened_projects : p.key => p }
-##ADDED 6/27/2025
-#  for_each = {
-#    for p in local.flattened_projects : p.key => p
-#    if p.private_subnets_per_vpc > 0 || p.public_subnets_per_vpc > 0
-#  }
-
-#  cidr = var.vpc_cidr_block
-#  azs  = data.aws_availability_zones.available.names
-#  private_subnets = slice(var.private_subnet_cidr_blocks, 0, each.value.private_subnets_per_vpc)
-#  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, each.value.public_subnets_per_vpc)
-#  enable_nat_gateway      = true
-#  enable_vpn_gateway      = false
-#  map_public_ip_on_launch = false
-#}
 
 #NEW APP SECURITY GROUP 6/27/2025
 module "app_security_group" {
@@ -118,10 +98,6 @@ module "lb_security_group" {
     if p.private_subnets_per_vpc > 0 || p.public_subnets_per_vpc > 0
   }
 
-#for_each = {
-#    for k, v in var.project : k => v
-#    if try(module.vpc[k].vpc_id, null) != null
-#  }
 
   name = "load-balancer-sg-${each.key}-${each.value.environment}"
   description = "Security group for load balancer with HTTP ports open within VPC"
@@ -138,25 +114,7 @@ resource "random_string" "lb_id" {
   special = false
 }
 
-#NEW ELB with name:
-#locals {
-#  elb_names = {
-#    for k, v in var.project : k =>
-#    k == "maps"
-#    ? "maps.demo.pabrennan.com"
-#    : trimsuffix(
-#        substr(
-#          join(
-#            "",
-#            regexall("[a-zA-Z0-9-]", join("-", ["lb", random_string.lb_id.result, k, v.environment]))
-#          ),
-#          0,
-#          32
-#        ),
-#        "-"
-#      )
-#  }
-#}
+
 
 #ADDED NEW ELB MODULE
 module "elb_http" {
@@ -234,21 +192,50 @@ module "ec2_instances" {
   source     = "./modules/aws-instance"
   depends_on = [module.vpc]
 
-#Added 6/27/2025
-#  for_each = { for p in local.flattened_projects : p.key => p }
   for_each = {
     for p in local.flattened_projects : p.key => p
-    if p.private_subnets_per_vpc > 0
+    if p.private_subnets_per_vpc > 0 || p.public_subnets_per_vpc > 0
   }
 
-  instance_count     = each.value.instances_per_subnet * length(module.vpc[each.key].private_subnets)
+  instance_count = each.value.instances_per_subnet * (
+    each.value.private_subnets_per_vpc > 0
+      ? length(module.vpc[each.key].private_subnets)
+      : length(module.vpc[each.key].public_subnets)
+  )
+
+  subnet_ids = (
+    each.value.private_subnets_per_vpc > 0
+      ? module.vpc[each.key].private_subnets
+      : module.vpc[each.key].public_subnets
+  )
+
   instance_type      = each.value.instance_type
-  subnet_ids         = module.vpc[each.key].private_subnets[*]
   security_group_ids = [module.app_security_group[each.key].security_group_id]
-  project_name = each.key
-  environment  = each.value.environment
-  ssh_key_name = var.ssh_key_name   # Add this line
+  project_name       = each.key
+  environment        = each.value.environment
+  ssh_key_name       = var.ssh_key_name
 }
+
+
+#module "ec2_instances" {
+#  source     = "./modules/aws-instance"
+#  depends_on = [module.vpc]
+
+#Added 6/27/2025
+#  for_each = { for p in local.flattened_projects : p.key => p }
+#  for_each = {
+#    for p in local.flattened_projects : p.key => p
+#    if p.private_subnets_per_vpc > 0
+#  }
+
+#  instance_count     = each.value.instances_per_subnet * length(module.vpc[each.key].private_subnets)
+#  instance_type      = each.value.instance_type
+#  subnet_ids         = module.vpc[each.key].private_subnets[*]
+#  security_group_ids = [module.app_security_group[each.key].security_group_id]
+#  project_name = each.key
+#  environment  = each.value.environment
+#  ssh_key_name = var.ssh_key_name   # Add this line
+#}
 
 #ADDED 6/26/2025
 resource "aws_key_pair" "deployer" {
